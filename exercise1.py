639a2c49c8bf3e9db8fd28d96fb1670350b699e6
vamsikota2012 <39314690+vamsikota2012@users.noreply.github.com>
2019-12-10 13:22:37 -0500

    Create exercise1.py

---
+++ exercise1.py
@@ -0,0 +1,7 @@
+import datetime
+x = datetime.datetime.now()
+name = input("Enter your name: ")
+age = input("enter your age: ")
+print("Hi " + name + " How are you doing")
+y = 100 - int(age)
+print(name + " you will turn 100 years on " + str(int(x.year)+y))
